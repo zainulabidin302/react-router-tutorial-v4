c30ba4eaf1a335319ef30c329fa78dd6d07d26b2
Zain <zain302@hotmail.com>
2017-06-05 18:34:39 +0500

    introduction finalized

--- introduction/src/App.js
+++ introduction/src/App.js
@@ -2,33 +2,20 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css';
 
-import About from './Components/About';
-import Header from './Components/Header';
 import Home from './Components/Home';
-import Pricing from './Components/Pricing';
+
 import background from './assets/background.jpg'
-
-
 
 
 class App extends Component {
   render() {
     return (
 
-          <BrowserRouter>
             <div className="App"  style={{backgroundImage: `url(${background})`, backgroundSize: `cover`}} >
-              <div className="App-header">
-                <Header  />
-              </div>
               <div className="App-Body">
-                <Switch>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/pricing" component={Pricing} />
-                </Switch>
+                <Home />
               </div>
             </div>
-
-          </BrowserRouter>
 
     );
   }
