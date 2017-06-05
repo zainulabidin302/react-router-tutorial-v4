300268156c093fb14bece1d172c263a72008c220
Zain <zain302@hotmail.com>
2017-06-05 18:19:24 +0500

    initial routes home and pricing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Home = () => (
-  <div>Home</div>
+  <div>
+    <h3> Welcome to Home! </h3>
+    <article>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quam sapien, lobortis at maximus ut, fringilla at tortor. Curabitur dapibus, elit in egestas dignissim, sem tortor dictum sem, et bibendum dolor augue non purus. Sed libero felis, hendrerit eu auctor at, tempor nec justo. Mauris accumsan libero ut ligula finibus consequat. Quisque posuere metus id sem rutrum, sit amet convallis libero dictum. Pellentesque id malesuada dolor. Nullam efficitur turpis et nibh fringilla eleifend. Duis dignissim condimentum tortor nec dictum. In sit amet ullamcorper lorem. Pellentesque nec elit congue urna cursus egestas. Nunc venenatis felis dui, id tempor lacus suscipit non. Integer vehicula, nibh nec fermentum commodo, mauris nunc semper lorem, sit amet tincidunt velit magna ut sem. In rutrum, odio ut suscipit elementum, turpis nunc lobortis libero, sit amet dignissim neque eros nec urna. Fusce porttitor at purus ac vestibulum. Etiam sem nunc, eleifend non sodales quis, egestas id elit. Pellentesque efficitur, ipsum in tempus finibus, elit diam viverra ex, eu ullamcorper felis metus nec elit.
+
+      Cras dictum pretium leo et tempor. Maecenas eget vulputate neque. Aenean rutrum scelerisque enim vitae tristique. In hac habitasse platea dictumst. Quisque et rhoncus risus, non condimentum tellus. Fusce cursus, orci sed volutpat semper, dolor mi ultricies lorem, eu tristique urna elit ac nisl. Donec dignissim suscipit mattis. In ac nibh dui. Nam vulputate lorem lacus, ultricies dignissim purus tempor sed. Nam efficitur, erat a tristique interdum, ex ipsum hendrerit sapien, vel molestie risus velit sed mi.
+
+      Vestibulum risus turpis, pharetra at lorem vel, gravida luctus tellus. Integer ornare convallis rhoncus. Quisque aliquet posuere lacus, ac tristique leo. Nullam eget laoreet nibh. Pellentesque non nisi ac turpis laoreet interdum. Maecenas interdum consectetur dolor, id pulvinar elit malesuada at. Cras ultrices, leo et rutrum malesuada, urna libero ullamcorper diam, id vehicula risus dui sit amet augue. In dictum ultrices pretium. Proin porttitor nulla cursus nibh volutpat, nec ullamcorper ipsum consequat. Nulla dignissim porta turpis nec auctor. Quisque porttitor, lacus sit amet bibendum efficitur, nisi lectus venenatis dui, ac pellentesque felis libero ac ligula. Aliquam pulvinar pretium massa, vel consequat nulla. Ut nec felis quis ex vehicula porta. Fusce cursus congue ante, ac bibendum turpis tempor vel.
+    </article>
+  </div>
 );
 
 export default Home;
