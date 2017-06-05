2a6078deaab4442211ebcb478d41cade188cc336
Zain <zain302@hotmail.com>
2017-06-05 18:25:15 +0500

    new folders

---
+++ declaring-routes/src/Components/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <div>About</div>
+);
+
+export default App;
---
+++ introduction/src/Components/Pricing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Pricing = ({data}) => (
+  <div>
+    <h3>Pricing Table will be here shortly!</h3>
+  </div>
+);
+
+
+export default Pricing;
